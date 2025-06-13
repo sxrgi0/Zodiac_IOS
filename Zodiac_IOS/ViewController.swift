c14e775a7e002d9ea674313b9f766558653dd6a8
sxrgi0 <sergiofh.2002@gmail.com>
2025-06-13 16:53:13 +0200

    Initial Commit

---
+++ Zodiac_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zodiac_IOS
+//
+//  Created by Tardes on 13/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
